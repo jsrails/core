f43d99dbf1b203e1f31b0f8eb0c4a3a96f892cef
Vitaliy <theyamshikov@yandex.ru>
2019-10-25 22:42:30 +0600

    refacto

--- gulp/config/src.js
+++ gulp/config/src.js
@@ -1,17 +1,19 @@
 
 var src = {
-    vendorRequired: [
-        './node_modules/lodash/lodash.min.js',
-    ],
-    railsRequired: [
-        './node_modules/jrails/kernel/namespace.js',
-        './node_modules/jrails/kernel/container.js',
-        './node_modules/jrails/kernel/bootstrap.js',
-        './node_modules/jrails/kernel/func.js',
+    script: {
+        vendorRequired: [
+            './node_modules/lodash/lodash.min.js',
+        ],
+        railsRequired: [
+            './node_modules/jrails/kernel/namespace.js',
+            './node_modules/jrails/kernel/container.js',
+            './node_modules/jrails/kernel/bootstrap.js',
+            './node_modules/jrails/kernel/func.js',
 
-        './node_modules/jrails/helper/*.js',
-        './node_modules/jrails/event/*.js',
-    ],
+            './node_modules/jrails/helper/*.js',
+            './node_modules/jrails/event/*.js',
+        ],
+    }
 };
 
 module.exports = src;
