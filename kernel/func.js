3af945e106d025998a9d32d02497dce64a55d4e7
Vitaliy <theyamshikov@yandex.ru>
2019-10-25 20:41:39 +0600

    init

---
+++ kernel/func.js
@@ -0,0 +1,11 @@
+space(function() {
+
+    /**
+     * Функция вывода данных в консоль
+     */
+    window.dump = function(value) {
+        console.log(value);
+    };
+    window.d = window.dump;
+
+});
