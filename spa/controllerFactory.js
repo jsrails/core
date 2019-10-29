e6d3cb02b7eb02688b32226a3b3cd4eba70674b8
Vitaliy <theyamshikov@yandex.ru>
2019-10-29 22:42:36 +0600

    make classes

---
+++ spa/controllerFactory.js
@@ -0,0 +1,43 @@
+define(['jrails/vue/vm', 'jrails/spa/layer', 'jrails/spa/query', 'lodash'], function(vm, spaLayer, spaQuery, _) {
+
+    var initTemplate = function (controller, templateHtml) {
+        spaLayer.hideAll();
+        //if( ! vm.has(controller.el)) {
+            var id = controller.el.replace('#', '');
+            spaLayer.add222(templateHtml, id);
+            vm.ensure(controller);
+        //}
+        $(controller.el).show();
+        if(_.isFunction(controller.onShow)) {
+            controller.onShow();
+        }
+    };
+
+    var loadTemplate = function (controller, templateFileName) {
+        require([templateFileName], function (templateHtml) {
+            initTemplate(controller, templateHtml);
+        });
+    };
+
+    var onReady = function (controller) {
+        if(_.isFunction(controller.onReady)) {
+            controller.onReady();
+        }
+        if( ! _.isEmpty(controller.templateFile)) {
+            var templateFileName = 'text!' + controller.templateFile;
+            loadTemplate(controller, templateFileName);
+        }
+    };
+
+    return {
+        loadTemplate: function (controller, templateFileName) {
+            loadTemplate(controller, templateFileName);
+        },
+        createByClassName: function (controllerClassName, query) {
+            spaQuery.set(query);
+            define('query', [], );
+            require([controllerClassName], onReady);
+        }
+    };
+
+});
